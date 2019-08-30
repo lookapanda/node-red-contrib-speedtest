a5814abe5a2f24bc2664e5102d6560da2bf361bd
Simon <sn@hitbox.tv>
2019-08-31 01:25:16 +0200

    Update speedtest.js

--- speedtest.js
+++ speedtest.js
@@ -1,33 +1,24 @@
 'use strict';
 
-var speedTest = require('speedtest-net');
+var SpeedTest = require('fast-speedtest-api');
 
 module.exports = exports = function(RED) {
     function SpeedTest(config) {
-        var timeout = config.maxTime || 5 * 1000;
         RED.nodes.createNode(this, config);
 
         this.on('input', msg => {
+            if (!config.token) {
+                this.status({ fill: 'red', shape: 'dot', text: 'You must define app token' });
+                return;
+            }
             this.status({ fill: 'yellow', shape: 'dot', text: 'Requesting' });
-            var test = speedTest({ maxTime: config.maxTime, serverId: config.serverId });
-
-            test.on('data', data => {
-                var reponse = Object.assign({}, data, { config: config });
+            var test = new SpeedTest({ token: config.token, unit: SpeedTest.UNITS.Mbps });
+            
+            test.getSpeed().then(s => {
                 this.status({});
-                this.send({ payload: reponse });
-            });
-
-            test.on('error', err => {
-                this.status({ fill: 'red', shape: 'dot', text: err.message });
-                this.error(err, msg);
-            });
-
-            test.on('downloadspeedprogress', speed => {
-                this.send({ topic: 'downloadprogress', payload: speed });
-            });
-
-            test.on('uploadspeedprogress', speed => {
-                this.send({ topic: 'uploadprogress', payload: speed });
+                this.send({ payload: s });
+            }).catch(e => {
+                this.status({ fill: 'red', shape: 'dot', text: e.message });
             });
         });
     }
